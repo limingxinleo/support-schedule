2ba75b292c1a259b6d0d650d4e4296a2894c5a71
李铭昕 <715557344@qq.com>
2017-08-01 18:28:13 +0800

    * 增加第一个版本

--- src/Support/Schedule.php
+++ src/Support/Schedule.php
@@ -10,5 +10,41 @@ namespace limx\Support;
 
 class Schedule
 {
+    // 分钟[0-59] 小时[0-23] 天[1-31] 月[1-12] 星期几[0-6]
+    public $expression = '* * * * *';
+
+    protected $now = '';
+
+    public function __construct($date = null)
+    {
+        $this->now = isset($date) ? $date : date('i H d m w');
+    }
+
+    public function cron($expression)
+    {
+        $expected = explode(' ', $expression);
+        $actual = explode(' ', $this->now);
+        foreach ($expected as $i => $v) {
+            if ($v != '*' && intval($expected[$i]) !== intval($actual[$i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function daily()
+    {
+        return $this->cron('0 0 * * *');
+    }
+
+    public function dailyAt($hour, $minute)
+    {
+        return $this->cron("{$minute} {$hour} * * *");
+    }
+
+    public function everyMinute()
+    {
+        return $this->cron("* * * * *");
+    }
 
 }
--- tests/Schedule/BaseTest.php
+++ tests/Schedule/BaseTest.php
@@ -17,4 +17,44 @@ class BaseTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testCronCase()
+    {
+        $date = '40 09 01 08 2';
+        $schedule = new Schedule($date);
+        $this->assertTrue($schedule->cron('* * * * *'));
+        $this->assertTrue($schedule->cron('40 * * * *'));
+        $this->assertTrue($schedule->cron('40 * 01 * *'));
+        $this->assertTrue($schedule->cron('* * 01 08 02'));
+    }
+
+    public function testDailyCase()
+    {
+        $date = '0 0 01 08 2';
+        $schedule = new Schedule($date);
+        $this->assertTrue($schedule->daily());
+
+        $date = '3 0 01 08 2';
+        $schedule = new Schedule($date);
+        $this->assertFalse($schedule->daily());
+    }
+
+    public function testDailyAtCase()
+    {
+        $date = '0 0 01 08 2';
+        $schedule = new Schedule($date);
+        $this->assertFalse($schedule->dailyAt(0, 3));
+
+        $date = '3 0 04 08 2';
+        $schedule = new Schedule($date);
+        $this->assertTrue($schedule->dailyAt(0, 3));
+    }
+
+    public function testEveryMinuteCase()
+    {
+        $date = '33 10 01 08 2';
+        $schedule = new Schedule($date);
+        $this->assertTrue($schedule->everyMinute());
+    }
+
 }
